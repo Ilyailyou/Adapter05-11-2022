8ea93c32271c314fcb477fa031275cc145f66766
Ilyailyou <yaaponia@gmail.com>
2022-11-05 22:57:18 +0500

    Initial commit=)

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        Ints calc = new IntsCalculator();
+        System.out.println(calc.sum(5, 7652));
+        System.out.println(calc.mult(454, 81));
+        System.out.println(calc.pow(3, 19));
+
+    }
+}
